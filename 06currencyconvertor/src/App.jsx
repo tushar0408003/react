7831162aae2a6a8f7da660155a71c559991a4aac
tushar jakhar <tusharjakhar001@gmail.com>
2025-04-19 11:56:06 +0530

    chai aur code

---
+++ 06currencyconvertor/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text-3xl bg-green-500">currency convertor</h1>
+       
+    </>
+  )
+}
+
+export default App
